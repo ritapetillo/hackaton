4ce76710045513a043755b1b23e1b417b13252bb
bolketdev <42487810+ritapetillo@users.noreply.github.com>
2022-03-25 17:09:18 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,6 @@ module.exports = {
   entry: __dirname + "/src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.bundle.js",
+    filename: "main.bundle.js",
   },
 };
